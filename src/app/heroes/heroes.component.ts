3e9147b893dba77caa18273879e0b0b29422893c
Amrez <amrezb2c@gmail.com>
2023-04-15 22:56:08 +0500

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent {
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm',
+  // };
+  heroes = HEROES;
+  //variable//
+  selectedHero?: Hero;
+  //variable//
+  //onClick fuction//
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    //onClick fuction//
+  }
+}
